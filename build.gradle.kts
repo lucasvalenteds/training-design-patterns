bd420b427c6e8b22b3aa571c8ee139fe653325fa
Lucas <lucasvalenteds@gmail.com>
2019-09-25 08:31:20 -0300

    Setup Gradle for multi-module project

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    java
+}
+
+subprojects {
+    apply(plugin = "java")
+
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        testImplementation("org.junit.jupiter", "junit-jupiter", properties["version.junit"].toString())
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+        testLogging {
+            events = setOf(TestLogEvent.PASSED, TestLogEvent.FAILED)
+        }
+    }
+}
